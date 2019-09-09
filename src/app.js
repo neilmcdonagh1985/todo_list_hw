37b42277f095d2445bf1e487b526e8da29a9ff24
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2019-09-09 21:45:53 +0100

    mvp done

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Vue({
+        el:"#app",
+        data: {
+            todos: [
+                {task: "buy shopping", done: false},
+                {task: "clean bathroom", done: true},
+                {task: "car's MOT", done: false}
+            ],
+            newTodo: ""
+        },
+
+        methods: {
+            addNewItem: function() {
+                this.todos.push({
+                    task: this.newTodo,
+                    done: false
+                });
+                this.newTodo = "";  
+            },
+            doTask: function(index) {
+                this.todos[index].done = true;
+            }
+
+        }
+    }) 
+})
